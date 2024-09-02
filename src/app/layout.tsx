c226e8434d4f5d16b5765442a3f6c1ac10219376
namassist <chairulanmm@gmail.com>
2024-09-02 09:19:44 +0700

    [fix] favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,12 +4,14 @@ import "./globals.css";
 
 import NextTopLoader from "nextjs-toploader";
 import { ThemeProvider } from "next-themes";
+import CustomHead from "./CustomHead";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "PT Kilang Pertamina Internasional RU VII Kasim",
   description: "Landing page PT Kilang Pertamina Internasional RU VII Kasim ",
+  icons: [{ rel: "icon", url: "/logo.ico" }],
 };
 
 export default function RootLayout({
@@ -19,12 +21,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-        <link rel="icon" href="/logo.ico" sizes="any" />
-      </head>
       <body className={`${inter.className} bg-background`}>
         <ThemeProvider defaultTheme="dark">
           <NextTopLoader color="#5A8ED4" />
+          <CustomHead />
           {children}
         </ThemeProvider>
       </body>
